04571f2fb0ce6f0907a6ae141c00e3b2ea043eb8
Isaac <isahowayu4@gmail.com>
2022-09-07 09:55:03 -0500

    ENTREGABLE

---
+++ Primer-Parcial/Practica-1/ENTREGABLE/Ejercicio-1.py
@@ -0,0 +1,136 @@
+"""
+    INSTITUTO POLITECNICO NACIONAL
+    ESCUELA SUPERIOR DE CÓMPUTO
+    ALUMNOS:
+        - SÁNCHEZ VERDIGUEL ISAAC
+        - TREVIÑO PALACIOS AXEL 
+    GRUPO: 3CV11
+    MATERIA: ANALISIS DE ALGORITMOS
+    PROFESOR: BENJAMIN LUNA BENOSO
+    FECHA: 07 - 09 - 2022
+    PRACTICA 1: ALGORITMO BUSQUEDA COINCIDENCIA ARREGLOS
+    
+
+
+"""
+
+
+import random
+import csv
+
+contador = 0
+
+# GENERADORES
+# Genera el arreglo para el caso normal
+
+
+def generarArreglo(n):
+    resultado = []
+    for i in range(0, n):
+        resultado.append(random.randrange(0, 3*n))
+        i += 1
+    return resultado
+
+# Genera el arreglo para el mejor caso
+
+
+def mejorCaso(n):
+    arreglo = generarArreglo(n)
+    # El cual tiene el mismo elemento al inicio y en la mitad
+    arreglo[int(len(arreglo)/2)] = arreglo[0]
+    return arreglo
+
+# Genera el arreglo para el peor caso
+
+
+def peorCaso(n):
+    resultado = []
+    # El cual no tiene ningún elemento compartido entre las dos mitades (por eso se generan en dos diferentes fors)
+    for i in range(0, int(n/2)):
+        resultado.append(random.randrange(0, int(1.5*n)))
+        i += 1
+    for i in range(int(n/2), n):
+        resultado.append(random.randrange(int(1.6*n), 3*n))
+        i += 1
+    return resultado
+
+# ALGORITMO
+
+
+def detectar(arreglo):
+    global contador
+
+    contador += 1
+    salir = False
+
+    # Dividir el arreglo en dos mitades
+    contador += 1
+    result = [False]
+    contador += 1
+    m1 = arreglo[0: int(len(arreglo)/2)]
+    contador += 1
+    m2 = arreglo[int(len(arreglo)/2):len(arreglo)]
+    contador += 1
+    i = 0
+
+    contador += 1
+    for e1 in m1:
+        contador += 1
+        contador += 1
+        j = 0
+        contador += 1
+        # Iteramos la segunda mitad para cada elemento de la primera mitad
+        for e2 in m2:
+            contador += 1
+            # Si se encuentra el elemento en común entonces se guarda y se sale del primer for
+            if e1 == e2:
+                contador += 1
+                contador += 1
+                result[0] = True
+                contador += 1
+                result.append(e1)
+                contador += 1
+                result.append(i)
+                contador += 1
+                result.append(j+int(len(arreglo)/2))
+                contador += 1
+                salir = True
+                contador += 1
+                break
+            contador += 1
+            contador += 1
+            j += 1
+        i += 1
+        contador += 1
+        contador += 1
+        # Si se encontró el elemento en común tambien se sale del segundo for
+        if salir:
+            contador += 1
+            contador += 1
+            break
+    contador += 1
+    return result
+
+
+resultados = []
+
+for i in range(2, 100):
+    arreglo = generarArreglo(i)
+    # arreglo = mejorCaso(i)
+    # arreglo = peorCaso(i)
+
+    resultado = detectar(arreglo)
+    print(arreglo)
+    if (resultado[0]):
+        print(
+            f"El elemento {resultado[1]} está repetido en [{resultado[2]},{resultado[3]}]")
+    else:
+        print("Las dos mitades no tienen elementos en común")
+
+    resultados.append([i, contador])
+    contador = 0
+
+with open('data.csv', 'w') as f:
+    writer = csv.writer(f)
+    for data in resultados:
+        writer.writerow(data)
---
+++ Primer-Parcial/Practica-1/ENTREGABLE/Ejercicio-2.py
@@ -0,0 +1,69 @@
+"""
+    INSTITUTO POLITECNICO NACIONAL
+    ESCUELA SUPERIOR DE CÓMPUTO
+    ALUMNOS:
+        - SÁNCHEZ VERDIGUEL ISAAC
+        - TREVIÑO PALACIOS AXEL 
+    GRUPO: 3CV11
+    MATERIA: ANALISIS DE ALGORITMOS
+    PROFESOR: BENJAMIN LUNA BENOSO
+    FECHA: 07 - 09 - 2022
+    PRACTICA 1: ALGORITMO EUCLIDES
+    
+
+
+"""
+
+
+import csv
+import math
+import random
+
+contador = 0
+
+
+def Euclides(m, n):
+    global contador
+
+    # Mientras haya un residuo
+    while n != 0:
+        contador += 1
+        # Continuar con el residuo y el segundo número
+        contador += 1
+        r = m % n
+        contador += 1
+        m = n
+        contador += 1
+        n = r
+    contador += 1
+    return m
+
+
+resultados = []
+
+# Caso Normal
+# En el caso normal se usarán datos al azar entre 2 y el número máximo del peor caso
+for i in range(500):
+    m = random.randrange(2, 20365011074)
+    n = random.randrange(2, 20365011074)
+
+    contador = 0
+    Euclides(m, n)
+    print(f"({m}, {n}) -> {contador}")
+    resultados.append([max(m, n), contador])
+
+# Peor caso
+# En el peor caso se usan los primeros 50 valores de la serie Fibonacci
+# m = n = 1
+# for i in range(50):
+#     contador = 0
+#     Euclides(m, n)
+#     print(f"({m}, {n}) -> {contador}")
+#     resultados.append([n, contador])
+#     n += m
+#     m = n - m
+
+with open('data.csv', 'w') as f:
+    writer = csv.writer(f)
+    for data in resultados:
+        writer.writerow(data)
